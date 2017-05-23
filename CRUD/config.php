35e5c03baeaaf9333706217e0ba5a845ee7f7817
Renato Fiorezze <renato.fiorezze@hotmail.com>
2017-05-23 09:28:50 -0300

    Update config.php

--- CRUD/config.php
+++ CRUD/config.php
@@ -1,7 +1,7 @@
 <?php
 	define ('DB_HOSTNAME','mysql.hostinger.com.br');
 	define ('DB_USERNAME','u642936760_root');
-	define ('DB_PASSWORD', 'Rf164455');
+	define ('DB_PASSWORD', 'password');
 	define ('DB_DATABASE','u642936760_volei');
 	define ('DB_PREFIX','tb');
 	define ('DB_CHARSET','utf8');
